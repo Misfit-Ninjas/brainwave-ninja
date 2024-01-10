b22d540759c7c62dc33dc62fb8b191f658b1d53d
Peter Naudus <uselinux@gmail.com>
2024-01-10 01:37:55 -0500

    Refactored quarters to make it more streaming

--- backend/services/neurons/financial/quarters.py
+++ backend/services/neurons/financial/quarters.py
@@ -21,18 +21,13 @@ class Results(TypedDict):
 
 
 def run(config: Config, symbol_results: Iterator[symbols.Results]) -> Iterator[Results]:
-    result = set()
     for symbol in symbol_results:
         for balance_sheet in fmp.balance_sheet.quarter(symbol["symbol"]):
-            result.add(
-                (
-                    balance_sheet["calendar_year"],
-                    balance_sheet["period"],
-                    f"{balance_sheet['calendar_year']}-{balance_sheet['period']}",
-                )
-            )
-    for calendar_year, period, display in sorted(result):
-        if (not config.start_quarter or config.start_quarter <= display) and (
-            not config.end_quarter or display <= config.end_quarter
-        ):
-            yield {"calendar_year": calendar_year, "period": period, "display": display}
+            calendar_year = balance_sheet["calendar_year"]
+            period = balance_sheet["period"]
+            display = f"{balance_sheet['calendar_year']}-{balance_sheet['period']}"
+
+            if (not config.start_quarter or config.start_quarter <= display) and (
+                not config.end_quarter or display <= config.end_quarter
+            ):
+                yield {"calendar_year": calendar_year, "period": period, "display": display}
